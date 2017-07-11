4f1e370d95e1677ec21281cbc282f7295acc7af0
Naomi Villanueva <naovillaj@gmail.com>
2017-07-10 21:31:46 -0500

    py

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function(){
-  $(".button-collapse").sideNav();
-    // $('.parallax').parallax();
-    $('.scrollspy').scrollSpy();
-    $('.carousel').carousel();
+  $(".button-collapse").sideNav();  
+  // $('.parallax').parallax();
+  $('.scrollspy').scrollSpy();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $('.carousel').carousel();
+  setInterval(function() {
+    $('.carousel').carousel('next');
+  }, 2000);
+
 });
