16f11a165178e752abfb63cdc7932a44ede1977f
Naomi Villanueva <naovillaj@gmail.com>
2017-07-17 16:20:04 -0500

    linkcv

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,9 +11,19 @@ $(document).ready(function(){
 
 
   $('.move-scroll').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top -70}, 800, 'linear');
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top -70}, 800, 'linear');
   });
+
+  $('.nav-click').on('click', function(e){
+	e.preventDefault();
+	$('.side-nav').hide();	
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top -70}, 800, 'linear');
+    $('#sidenav-overlay').css('background-color', 'transparent');
+  });
+  $('.button-collapse').on('click', function(){
+  	$('.side-nav').show();
+  })
 
   new WOW().init();
 
